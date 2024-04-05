84cce971dfa43dd7c10822d07df8d5836bfddacb
Roshan <roshanadhikari66666@gmail.com>
2024-04-05 13:51:09 +0545

    Commited

---
+++ frontend/src/Homepages/Notifications.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notifications() {
+  return (
+    <div className="  bg-red-800 ml-20">
+
+    </div>
+  )
+}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,6 +10,7 @@ import { useEffect } from 'react';
 import { loginRoute } from '../../utils/ApiRoutes';
 import{ToastContainer,toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import sideimg from '../assets/auth.png'
 function Login() {
   const toastOptions = {
     position: "bottom-right",
@@ -30,7 +31,7 @@ useEffect(()=>{
   errors.Password?toast.error(errors.Password.message,toastOptions):" ";
    },[errors])
 const submit=async (values)=>{
-  
+
   const{Username,Password}=values;
 
   const {data}=await axios.post(loginRoute,{Username,Password});
@@ -46,24 +47,29 @@ toast.error(data.msg,toastOptions);
   
 
   return (
-    <section>
-   <div className=" border-solid border-2 w-96 h-96 absolute right-[400px] top-14 flex-col justify-center align-middle   ">
+    <section className='flex justify-center'>
+      <div className="h-44  mt-12 sm:block hidden">
+<img src={sideimg} className="h-[35rem] w-[25rem]"/>
+      </div>
+    <div className="flex flex-col gap-5 self-center">
+   <div className="m-auto mt-20 border-solid border-2 w-80 h-96 right-[400px]  flex-col justify-center align-middle   ">
     <div>
-    <img src={Logo} className=" w-56 my-0 mx-20    cursor-grab  transition-all hover:opacity-80" onMouseOverCapture={()=>{
+    <img src={Logo} className=" w-56 my-0 mx-[3rem]   cursor-grab  transition-all hover:opacity-80" onMouseOverCapture={()=>{
       setLogo(Logo2);
     }} onMouseOutCapture={()=>{setLogo(Logo1)}}/>
     </div>
     <form onSubmit={handleSubmit(submit)}>
-   <input type="text" className=" border-solid border-2 w-72 h-9 mx-12 mb-1 border-gray-300 rounded bg-gray-200 pl-3" placeholder="Username" {...register("Username")}/> 
-   <input type="password" className=" border-solid border-2 w-72 h-9 mx-12 mb-3 border-gray-300 rounded bg-gray-200 pl-3" placeholder="Password" {...register("Password")}/> 
-  <button className="mx-12 w-72 h-8 text-white rounded mb-3 bg-blue-500 bg-opacity-80">Log in</button>
+   <input type="text" className=" border-solid border-2 w-60 h-9 mx-12 mb-1 border-gray-300 rounded bg-gray-200 pl-3" placeholder="Username" {...register("Username")}/> 
+   <input type="password" className=" border-solid border-2 w-60 h-9 mx-12 mb-3 border-gray-300 rounded bg-gray-200 pl-3" placeholder="Password" {...register("Password")}/> 
+  <button className="mx-12 w-60 h-8 text-white rounded mb-3 bg-blue-500 bg-opacity-80">Log in</button>
 </form>
-   <div className="mx-12 w-72 text-center flex gap-3"><p className="bg-gray-500 border-b-2 w-32 h-[1px] relative top-3"/>OR<p className="bg-gray-500 border-b-2 w-32 h-[1px] relative top-3"/></div>
+   <div className="mx-10 w-60 text-center flex gap-3"><p className="bg-gray-500 border-b-2 w-32 h-[1px] relative top-3"/>OR<p className="bg-gray-500 border-b-2 w-32 h-[1px] relative top-3"/></div>
   
    </div>
-   <div className="text-center py-6 border-solid border-2 w-96 h-20 absolute right-[400px] bottom-56   ">
+   <div className="m-auto text-center py-6 border-solid border-2 w-80 h-20  bottom-56   ">
 Don't have a account? <button className="text-blue-400" onClick={()=>{Navigate("/register")}}>Sign up</button>
 </div>
+   </div>
    </section>
   )
 }
