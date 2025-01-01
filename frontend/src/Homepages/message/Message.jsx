b17811dbe06fc8ec934dfeba915a55491bc57f8a
Roshan <roshanadhikari66666@gmail.com>
2025-01-01 13:25:18 +0545

    progress

--- frontend/src/Homepages/message/Message.jsx
+++ frontend/src/Homepages/message/Message.jsx
@@ -14,14 +14,7 @@ export default function Message() {
 useEffect(()=>{
 dispatch(setUser(state.currentUser))
 },[])
-const data = [
-  {name:'Rey Jhon',time:'just now', message: 'Hey there! Are you finish creating the chat app?', active: true},
-  {name:'Cherry Ann',time:'12:00', message: 'Hello? Are you available tonight?'},
-  {name:'Lalaine',time:'yesterday', message: 'I\'m thingking of resigning'},
-  {name:'Princess',time:'1 day ago', message: 'I found a job :)'},
-  {name:'Charm',time:'1 day ago', message: 'Can you me some chocolates?'},
-  {name:'Garen',time:'1 day ago', message: 'I\'m the bravest of all kind'},
-]
+
 
   return (
     <div className="">
